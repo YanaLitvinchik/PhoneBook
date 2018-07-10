371ecada4c58eb40fe8514140b40b8ce158b89cc
student <student@K09-03>
2018-07-10 16:54:00 +0300

    Add project files.

---
+++ PhoneBook/Models/Contact.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PhoneBook.Models
+{
+    class Contact : INotifyPropertyChanged
+    {
+        private string person;
+        private string phone;
+        private string email;
+
+
+        public string Person
+        {
+            get { return person; }
+            set
+            {
+                person = value;
+                OnPropertyChanged("Person");
+            }
+        }
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                phone = value;
+                OnPropertyChanged("Phone");
+            }
+        }
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ PhoneBook/ViewModels/DataManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+using PhoneBook.Models;
+using System.Collections.ObjectModel;
+
+namespace PhoneBook.ViewModels
+{
+    class DataManager : INotifyPropertyChanged
+    {
+        private string path;
+        private XDocument doc;
+
+        private Contact selectedContact;
+        public Contact SelectedContact
+        {
+            get{ return selectedContact; }
+            set { selectedContact = value; OnPropertyChanged("SelectedContact");}
+        }
+        public ObservableCollection<Contact> Contacts { get; set; }
+
+        public DataManager()
+        {
+            Contacts = new ObservableCollection<Contact>();
+            path = @"..\..\Data\Contacts.xml";
+            doc = XDocument.Load(path);
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            var res = doc.Element("root").Elements("contact").ToList();
+            foreach (var x in res)
+            {
+                Contact c = new Contact()
+                {
+                    Person = x.Attribute("person").Value,
+                    Phone = x.Attribute("phone").Value,
+                    Email = x.Attribute("email").Value
+                };
+                Contacts.Add(c);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
